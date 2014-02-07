87efdd7b5c06f782891f5351e371c2b748a4edf3
Noon Silk <noonsilk@gmail.com>
2014-02-07 17:10:16 +1100

    Bits and pieces for special formatting.

--- moin/wiki/data/plugin/parser/description.py
+++ moin/wiki/data/plugin/parser/description.py
@@ -6,10 +6,15 @@ from MoinMoin.Page import Page
 from MoinMoin import wikiutil
 from MoinMoin.action import AttachFile
 
+from MoinMoin.parser.text_moin_wiki import Parser as WikiParser
+from MoinMoin.formatter.text_html import Formatter as HtmlFormatter
+
+
 class Parser:
     def __init__ (self, raw, request, **kw):
         self.raw = raw
         self.request = request
+        self.moin_parser = WikiParser(raw, request, **kw)
 
         self.out     = kw.get('out', request)
         attrs, msg   = wikiutil.parseAttributes(request, kw.get('format_args',''))
@@ -17,4 +22,12 @@ class Parser:
 
     def format (self, formatter):
         self.formatter = formatter
-        self.out.write(formatter.rawHTML(self.raw))
+        self.moin_parser.formatter = formatter
+
+        parser = WikiParser(self.raw, self.request, line_anchors=False)
+        formatter.startContent('')
+        output = self.request.redirectedOutput(parser.format, formatter)
+        formatter.endContent('')
+
+        output = '<div class="description"><hr />%s<hr /></div>' % output
+        self.out.write(output)
